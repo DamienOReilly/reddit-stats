62b1e1eab7ff7712f0b67260f3f156c49d67da0f
Damien O'Reilly <damienreilly@gmail.com>
2020-04-11 13:35:11 +0100

    adds snapshot functionality

---
+++ src/Codecs.elm
@@ -0,0 +1,198 @@
+module Codecs exposing (PostCount, PostCountPerDay, PostCountSubReddit, PushShiftData(..), PushShiftResult(..), User(..), deserializeSnapShot, postCountDecoder, postCountSubRedditDecoder, pushShiftAggDecoder, serializeSnapShot)
+
+import Base64
+import Bytes
+import Bytes.Decode as BytesDecode
+import Bytes.Encode as BytesEncode
+import Flate
+import Json.Decode as JsonDecode exposing (Decoder)
+import Json.Encode as JsonEncode
+import LZ77
+import Time exposing (Posix(..))
+import UrlBase64
+
+
+type User
+    = User String
+
+
+type PushShiftData
+    = RedditPostCountPerSubReddit (List PostCountSubReddit)
+    | RedditSubmissionCountPerSubReddit (List PostCountSubReddit)
+    | RedditPostCount (List PostCount)
+    | RedditSubmissionCount (List PostCount)
+
+
+type PushShiftResult
+    = PushShiftResult User Time.Posix (List PushShiftData)
+
+
+type alias PostCount =
+    { date : Int
+    , count : Int
+    }
+
+
+type alias PostCountPerDay =
+    { date : String
+    , count : Int
+    }
+
+
+type alias PostCountSubReddit =
+    { subreddit : String
+    , count : Int
+    }
+
+
+postCountDecoder : Decoder PostCount
+postCountDecoder =
+    JsonDecode.map2 PostCount
+        (JsonDecode.field "key" JsonDecode.int)
+        (JsonDecode.field "doc_count" JsonDecode.int)
+
+
+postCountEncoder : PostCount -> JsonEncode.Value
+postCountEncoder postCount =
+    JsonEncode.object
+        [ ( "key", JsonEncode.int postCount.date )
+        , ( "doc_count", JsonEncode.int postCount.count )
+        ]
+
+
+postCountSubRedditDecoder : Decoder PostCountSubReddit
+postCountSubRedditDecoder =
+    JsonDecode.map2 PostCountSubReddit
+        (JsonDecode.field "key" JsonDecode.string)
+        (JsonDecode.field "doc_count" JsonDecode.int)
+
+
+postCountSubRedditEncoder : PostCountSubReddit -> JsonEncode.Value
+postCountSubRedditEncoder postCountSubReddit =
+    JsonEncode.object
+        [ ( "key", JsonEncode.string postCountSubReddit.subreddit )
+        , ( "doc_count", JsonEncode.int postCountSubReddit.count )
+        ]
+
+
+pushShiftAggDecoder : Decoder a -> String -> Decoder (List a)
+pushShiftAggDecoder decoder aggregator =
+    JsonDecode.map identity (JsonDecode.field "aggs" (JsonDecode.field aggregator (JsonDecode.list decoder)))
+
+
+pushShiftAggEncoder : (a -> JsonEncode.Value) -> List a -> String -> JsonEncode.Value
+pushShiftAggEncoder encoderFn pushShiftData aggregator =
+    JsonEncode.object
+        [ ( "aggs"
+          , JsonEncode.object
+                [ ( aggregator, JsonEncode.list encoderFn pushShiftData )
+                ]
+          )
+        ]
+
+
+pushShiftEncoder : List PushShiftData -> JsonEncode.Value
+pushShiftEncoder pushShiftData =
+    pushShiftData
+        |> List.map
+            (\data ->
+                case data of
+                    RedditPostCount count ->
+                        ( "post_count", pushShiftAggEncoder postCountEncoder count "created_utc" )
+
+                    RedditSubmissionCount count ->
+                        ( "submission_count", pushShiftAggEncoder postCountEncoder count "created_utc" )
+
+                    RedditPostCountPerSubReddit count ->
+                        ( "post_count_per_subreddit", pushShiftAggEncoder postCountSubRedditEncoder count "subreddit" )
+
+                    RedditSubmissionCountPerSubReddit count ->
+                        ( "submission_count_per_subreddit", pushShiftAggEncoder postCountSubRedditEncoder count "subreddit" )
+            )
+        |> JsonEncode.object
+
+
+{-| Would like to now if its possible to do this without the intermediate type aliases
+-}
+type alias IntermediatePushShiftData =
+    { postCountPerSubReddit : List PostCountSubReddit
+    , submissionCountPerSubReddit : List PostCountSubReddit
+    , postCount : List PostCount
+    , submissionCount : List PostCount
+    }
+
+
+pushShiftDecoder : Decoder (List PushShiftData)
+pushShiftDecoder =
+    JsonDecode.map4 IntermediatePushShiftData
+        (JsonDecode.field "post_count_per_subreddit" (pushShiftAggDecoder postCountSubRedditDecoder "subreddit"))
+        (JsonDecode.field "submission_count_per_subreddit" (pushShiftAggDecoder postCountSubRedditDecoder "subreddit"))
+        (JsonDecode.field "post_count" (pushShiftAggDecoder postCountDecoder "created_utc"))
+        (JsonDecode.field "submission_count" (pushShiftAggDecoder postCountDecoder "created_utc"))
+        |> JsonDecode.map
+            (\x ->
+                [ RedditPostCountPerSubReddit x.postCountPerSubReddit
+                , RedditSubmissionCountPerSubReddit x.submissionCountPerSubReddit
+                , RedditPostCount x.postCount
+                , RedditSubmissionCount x.submissionCount
+                ]
+            )
+
+
+snapShotEncoder : PushShiftResult -> JsonEncode.Value
+snapShotEncoder (PushShiftResult (User user) time pushShiftData) =
+    JsonEncode.object
+        [ ( "user", JsonEncode.string user )
+        , ( "snapshot_time", JsonEncode.int <| Time.posixToMillis time )
+        , ( "data", pushShiftEncoder pushShiftData )
+        ]
+
+
+type alias IntermediatePushShiftResult =
+    { user : String
+    , snapShotTime : Int
+    , data : List PushShiftData
+    }
+
+
+snapShotDecoder : Decoder PushShiftResult
+snapShotDecoder =
+    JsonDecode.map3 IntermediatePushShiftResult
+        (JsonDecode.field "user" JsonDecode.string)
+        (JsonDecode.field "snapshot_time" JsonDecode.int)
+        (JsonDecode.field "data" pushShiftDecoder)
+        |> JsonDecode.map
+            (\x -> PushShiftResult (User x.user) (Time.millisToPosix x.snapShotTime) x.data)
+
+
+deserializeSnapShot : String -> Maybe PushShiftResult
+deserializeSnapShot input =
+    UrlBase64.decode base64ToPushShiftResult input
+        |> Result.toMaybe
+
+
+base64ToPushShiftResult : String -> Result String PushShiftResult
+base64ToPushShiftResult input =
+    Base64.toBytes input
+        |> Maybe.andThen Flate.inflate
+        |> Maybe.andThen (\buffer -> BytesDecode.decode (BytesDecode.string (Bytes.width buffer)) buffer)
+        |> Maybe.andThen (\payload -> Result.toMaybe (JsonDecode.decodeString snapShotDecoder payload))
+        |> Result.fromMaybe "Bad Data"
+
+
+serializeSnapShot : PushShiftResult -> Maybe String
+serializeSnapShot result =
+    UrlBase64.encode pushShiftResultToBase64 result
+        |> Result.toMaybe
+
+
+pushShiftResultToBase64 : PushShiftResult -> Result String String
+pushShiftResultToBase64 result =
+    Result.fromMaybe "Bad data"
+        << Base64.fromBytes
+        << Flate.deflateWithOptions (Flate.Dynamic (Flate.WithWindowSize LZ77.maxWindowSize))
+        << BytesEncode.encode
+        << BytesEncode.string
+        << JsonEncode.encode 0
+    <|
+        snapShotEncoder result
--- src/Main.elm
+++ src/Main.elm
@@ -8,29 +8,31 @@ import Chartjs.DataSets.Bar as BarData
 import Chartjs.DataSets.DoughnutAndPie as DoughnutAndPieData
 import Chartjs.DataSets.Line as LineData
 import Chartjs.Options as ChartOptions
+import Chartjs.Options.Legend as ChartLegend
 import Chartjs.Options.Title as ChartTitle
+import Codecs exposing (PostCount, PostCountPerDay, PostCountSubReddit, PushShiftData(..), PushShiftResult(..), User(..), deserializeSnapShot, postCountDecoder, postCountSubRedditDecoder, pushShiftAggDecoder, serializeSnapShot)
 import Color exposing (Color)
 import Constants
 import DateFormat
-import Debug exposing (todo)
+import Debug
 import Dict
 import Dict.Extra
 import Hotkeys exposing (onEnterSend)
 import Html exposing (Html, button, div, input, text)
 import Html.Attributes exposing (placeholder, value)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import Http
-import Json.Decode as JD
+import Json.Decode as JsonDecode
 import List
 import List.Extra
+import Maybe
 import Pure
 import Task exposing (Task)
 import Time exposing (Month(..), millisToPosix, toYear, utc)
+import Url
+import Url.Parser as UrlParser exposing ((<?>))
+import Url.Parser.Query as UrlParserQuery
 import Utils exposing (descending, sortByWith)
-
-
-type User
-    = User String
 
 
 type Aggregator
@@ -43,37 +45,13 @@ type Content
     | Submission
 
 
-type PushShiftData
-    = RedditPostCountPerSubReddit (List PostCountSubReddit)
-    | RedditSubmissionCountPerSubReddit (List PostCountSubReddit)
-    | RedditPostCount (List PostCount)
-    | RedditSubmissionCount (List PostCount)
-
-
-type alias PostCount =
-    { date : Int
-    , count : Int
-    }
-
-
-type alias PostCountPerDay =
-    { date : String
-    , count : Int
-    }
-
-
-type alias PostCountSubReddit =
-    { subreddit : String
-    , count : Int
-    }
-
-
 type Error
     = HttpError Http.Error
 
 
 type Msg
-    = GotResult (Result Error (List PushShiftData))
+    = GotResult (Result Error PushShiftResult)
+    | ChangeInput User
     | Search User
 
 
@@ -81,21 +59,42 @@ type Model
     = InputUser User
     | Loading
     | Failure
-    | Success (List PushShiftData)
+    | Success PushShiftResult
 
 
 type AxisData
     = AxisData (List String) (List Float)
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( InputUser <| User "", Cmd.none )
+init : String -> ( Model, Cmd Msg )
+init urlString =
+    Url.fromString urlString
+        |> Maybe.map
+            (\url ->
+                case extractSnapShotArgument url of
+                    Just (PushShiftResult user time data) ->
+                        ( Success (PushShiftResult user time data), Cmd.none )
+
+                    Nothing ->
+                        ( InputUser <| User "", Cmd.none )
+            )
+        |> Maybe.withDefault ( InputUser <| User "", Cmd.none )
+
+
+extractSnapShotArgument : Url.Url -> Maybe PushShiftResult
+extractSnapShotArgument location =
+    { location | path = "" }
+        |> UrlParser.parse (UrlParser.query (UrlParserQuery.string "snapshot"))
+        |> Maybe.withDefault Nothing
+        |> Maybe.andThen deserializeSnapShot
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg _ =
     case msg of
+        ChangeInput user ->
+            ( InputUser user, Cmd.none )
+
         Search (User user) ->
             if String.length user > 0 then
                 ( Loading, Task.attempt GotResult <| getRedditStatistics <| User user )
@@ -105,8 +104,8 @@ update msg _ =
 
         GotResult result ->
             case result of
-                Ok postCounts ->
-                    ( Success postCounts, Cmd.none )
+                Ok pushShiftResult ->
+                    ( Success pushShiftResult, Cmd.none )
 
                 Err _ ->
                     ( Failure, Cmd.none )
@@ -117,44 +116,54 @@ subscriptions _ =
     Sub.none
 
 
+inputName : User -> Html Msg
+inputName (User user) =
+    div [ Html.Attributes.class Pure.grid ] <|
+        [ input
+            [ Html.Attributes.autofocus True
+            , Html.Attributes.class "input-reddit"
+            , placeholder "User to search for."
+            , value user
+            , onInput <| ChangeInput << (\u -> User u)
+            , onEnterSend <| Search << (\u -> User u)
+            ]
+            []
+        , button
+            [ Html.Attributes.class Pure.button
+            , Html.Attributes.class "button-reddit"
+            , onClick (Search <| User user)
+            ]
+            [ text "Go!" ]
+        ]
+
+{-| Maybe.withDefault "" (serializeSnapShot (PushShiftResult user time pushShiftData))-} 
+
 view : Model -> Html Msg
 view model =
     case model of
         InputUser (User user) ->
-            div [ Html.Attributes.class Pure.grid ]
-                [ input
-                    [ Html.Attributes.autofocus True
-                    , Html.Attributes.class "input-reddit"
-                    , placeholder "User to search for."
-                    , value user
-                    , onEnterSend <| Search << (\u -> User u)
-                    ]
-                    []
-                , button
-                    [ Html.Attributes.class Pure.button
-                    , Html.Attributes.class "button-reddit"
-                    , onClick (Search <| User user)
-                    ]
-                    [ text "Go!" ]
-                ]
+            inputName (User user)
 
         Loading ->
             div [ Html.Attributes.class Pure.grid, Html.Attributes.class "loader" ] []
 
         Failure ->
             text "Something bad happened."
-
-        Success pushShiftData ->
-            chartConstructor pushShiftData
-                |> List.map
-                    (\chart ->
-                        div
-                            [ Html.Attributes.class (Pure.unit [ "1" ])
-                            , Html.Attributes.class (Pure.unit [ "md", "1", "2" ])
-                            ]
-                            [ Chart.chart [] chart ]
-                    )
-                |> div [ Html.Attributes.class Pure.grid ]
+        Success (PushShiftResult user time pushShiftData) ->
+            inputName user
+                :: div [] [ text  "" ]
+                :: [ chartConstructor pushShiftData
+                        |> List.map
+                            (\chart ->
+                                div
+                                    [ Html.Attributes.class (Pure.unit [ "1" ])
+                                    , Html.Attributes.class (Pure.unit [ "md", "1", "2" ])
+                                    ]
+                                    [ Chart.chart [] chart ]
+                            )
+                        |> div [ Html.Attributes.class Pure.grid ]
+                   ]
+                |> div []
 
 
 getContentCountPerSubReddit : List PostCountSubReddit -> AxisData
@@ -328,6 +337,11 @@ chartOptions title =
                     title
                 |> ChartTitle.setDisplay True
             )
+        |> ChartOptions.setLegend
+            (ChartLegend.defaultLegend
+                |> ChartLegend.setPosition ChartCommon.Bottom
+            )
+
 
 
 constructPieChartData : AxisData -> String -> List Color -> ChartData.Data
@@ -370,12 +384,12 @@ constructLineChartData (AxisData labels values) label bgColor bdColor =
         |> ChartData.addDataset (ChartData.LineData dataSet)
 
 
-main : Program () Model Msg
+main : Program String Model Msg
 main =
     Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
 
 
-getRedditStatistics : User -> Task Error (List PushShiftData)
+getRedditStatistics : User -> Task Error PushShiftResult
 getRedditStatistics (User user) =
     let
         u =
@@ -392,11 +406,16 @@ getRedditStatistics (User user) =
                 |> Task.map RedditSubmissionCount
             ]
     in
-    Task.sequence requests
-        |> Task.mapError HttpError
+    Time.now
+        |> Task.andThen
+            (\time ->
+                Task.sequence requests
+                    |> Task.map (\results -> PushShiftResult (User user) time results)
+                    |> Task.mapError HttpError
+            )
 
 
-getPushShiftData : User -> Aggregator -> Content -> JD.Decoder a -> Task Http.Error (List a)
+getPushShiftData : User -> Aggregator -> Content -> JsonDecode.Decoder a -> Task Http.Error (List a)
 getPushShiftData (User user) aggregator content decoder =
     let
         aggregatorType =
@@ -425,26 +444,7 @@ getPushShiftData (User user) aggregator content decoder =
         }
 
 
-postCountDecoder : JD.Decoder PostCount
-postCountDecoder =
-    JD.map2 PostCount
-        (JD.field "key" JD.int)
-        (JD.field "doc_count" JD.int)
-
-
-postCountSubRedditDecoder : JD.Decoder PostCountSubReddit
-postCountSubRedditDecoder =
-    JD.map2 PostCountSubReddit
-        (JD.field "key" JD.string)
-        (JD.field "doc_count" JD.int)
-
-
-pushShiftAggDecoder : JD.Decoder a -> String -> JD.Decoder (List a)
-pushShiftAggDecoder decoder aggregator =
-    JD.map identity (JD.field "aggs" (JD.field aggregator (JD.list decoder)))
-
-
-handleJsonResponse : JD.Decoder a -> Http.Response String -> Result Http.Error a
+handleJsonResponse : JsonDecode.Decoder a -> Http.Response String -> Result Http.Error a
 handleJsonResponse decoder response =
     case response of
         Http.BadUrl_ url ->
@@ -460,7 +460,7 @@ handleJsonResponse decoder response =
             Err Http.NetworkError
 
         Http.GoodStatus_ _ body ->
-            case JD.decodeString decoder body of
+            case JsonDecode.decodeString decoder body of
                 Err _ ->
                     Err (Http.BadBody body)
 
